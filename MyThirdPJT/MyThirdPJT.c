d620cfb6c42888a001c6298ef9ba431ebc7c0a7d
GRAY JUNG <nijg@naver.com>
2020-01-27 19:48:15 +0800

    test

--- MyThirdPJT/MyThirdPJT.c
+++ MyThirdPJT/MyThirdPJT.c
@@ -6,17 +6,20 @@ void Say_hello(); //prototyping , function declaration
 int main()
 {
 
+	int x, y, z;
+	x = 1, y = 2, z = 3;
+
 	for (int i = 1; i < 100; i++)
 	{
-		Say_hello();
+		Say_hello(i);
 	}
 
 	return 0;
 }
 
 
-void Say_hello() // function definition
+void Say_hello(int i) // function definition
 {
-	printf("HI!\n");
+	printf("HI!%d\n",i);
 	return;
 }
