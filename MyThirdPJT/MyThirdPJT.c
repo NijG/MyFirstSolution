b2fde82875e3383c4548edeb107bf4b35e800d7f
GRAY JUNG <nijg@naver.com>
2020-01-28 10:17:04 +0800

    d

--- MyThirdPJT/MyThirdPJT.c
+++ MyThirdPJT/MyThirdPJT.c
@@ -1,4 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <stdlib.h>
+
+void* malloc(size_t size);
+void free(void* ptr);
+void* realloc(void* ptr, size_t size);
 
 void Say_hello(); //prototyping , function declaration
 
@@ -8,6 +15,18 @@ int main()
 
 	int x, y, z;
 	x = 1, y = 2, z = 3;
+
+	int ccc;
+	scanf("%d", &ccc);
+
+
+
+
+	printf("\n\n\n\n\%d", ccc);
+
+
+
+
 
 	for (int i = 1; i < 100; i++)
 	{
