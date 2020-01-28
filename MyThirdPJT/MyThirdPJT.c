45133f8b968ffa6d5fb61681060cefa05e61ff9a
GRAY JUNG <nijg@naver.com>
2020-01-28 14:10:28 +0800

    test21

--- MyThirdPJT/MyThirdPJT.c
+++ MyThirdPJT/MyThirdPJT.c
@@ -1,44 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <limits.h>
 #include <stdlib.h>
-
-void* malloc(size_t size);
-void free(void* ptr);
-void* realloc(void* ptr, size_t size);
-
-void Say_hello(); //prototyping , function declaration
-
 
 int main()
 {
 
-	int x, y, z;
-	x = 1, y = 2, z = 3;
-
-	int ccc;
-	scanf("%d", &ccc);
-
-
-
-
-	printf("\n\n\n\n\%d", ccc);
-
-
-
-
-
-	for (int i = 1; i < 100; i++)
-	{
-		Say_hello(i);
+	for(int i=0;i<256;i++)
+	{ 
+		printf("%d=%c\n", i, i);
 	}
 
+	
 	return 0;
-}
-
-
-void Say_hello(int i) // function definition
-{
-	printf("HI!%d\n",i);
-	return;
 }
