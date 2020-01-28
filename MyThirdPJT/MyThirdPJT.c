f11d5426df2cbfaffc7ee01d35cc495e33e55fdd
GRAY JUNG <nijg@naver.com>
2020-01-28 15:09:15 +0800

    test

--- MyThirdPJT/MyThirdPJT.c
+++ MyThirdPJT/MyThirdPJT.c
@@ -1,17 +1,45 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
+#include <float.h>
+#include <stdbool.h>
+#include <complex.h>
 
 int main()
 {
+	//round off error(ex1)
+	float a, b;
+	a = 1.0E20f + 1.0f;
+	b = a - 1.0E20f;
+	printf("%f\n", b);
 
-	for(int i=0;i<256;i++)
-	{ 
-		printf("%d=%c\n", i, i);
+	//round off error(ex1)
+	float c;
+	c = 0.0f;
+	for (int i = 0; i < 101; i++)
+	{
+		c = c + 0.01f;
 	}
+	printf("%f\n", c);
 
 	
+	//overflow
+	float d = FLT_MAX;
+
+	d = d *100.0f;
+	printf("%f\n", d);
+
+
+
+	_Bool b1;
+	b1 = 0; //false
+	b1 = 1; //true
+
+	bool b2, b3;
+	b2 = true;
+	b3 = false;
+
+
+	printf("%d %d %d\n", b1, b2, b3);
+
+
 	return 0;
 }
