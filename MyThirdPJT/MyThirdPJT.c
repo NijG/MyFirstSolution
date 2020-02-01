55152dc37834d98295b25a8a776a004ea5c3ad41
GRAY JUNG <nijg@naver.com>
2020-02-01 10:20:01 +0800

    test

--- MyThirdPJT/MyThirdPJT.c
+++ MyThirdPJT/MyThirdPJT.c
@@ -1,45 +1,13 @@
 #include <stdio.h>
-#include <float.h>
-#include <stdbool.h>
-#include <complex.h>
-
 int main()
-{
-	//round off error(ex1)
-	float a, b;
-	a = 1.0E20f + 1.0f;
-	b = a - 1.0E20f;
-	printf("%f\n", b);
+{ 
+	char chVar = 'G';
+	int iVar = 10;
+	char *cp = &chVar;
+	*cp = 'K';
+	printf("chVar : 내용 = %c, 주소 = %p\n" , chVar , &chVar);
+	printf("cp:%p,*cp : 내용 = %c, &cp = %p\n",cp, *cp, &cp);
 
-	//round off error(ex1)
-	float c;
-	c = 0.0f;
-	for (int i = 0; i < 101; i++)
-	{
-		c = c + 0.01f;
-	}
-	printf("%f\n", c);
+	return 0; 
 
-	
-	//overflow
-	float d = FLT_MAX;
-
-	d = d *100.0f;
-	printf("%f\n", d);
-
-
-
-	_Bool b1;
-	b1 = 0; //false
-	b1 = 1; //true
-
-	bool b2, b3;
-	b2 = true;
-	b3 = false;
-
-
-	printf("%d %d %d\n", b1, b2, b3);
-
-
-	return 0;
 }
